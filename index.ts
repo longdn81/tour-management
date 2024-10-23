4e3661787ee1ed5136967920213156dc97c52bee
longdn81 <146925221+longdn81@users.noreply.github.com>
2024-10-24 01:27:08 +0700

    cau truc thu muc mvc

--- index.ts
+++ index.ts
@@ -1,26 +1,20 @@
-import express, { Express, Request, Response } from "express";
-import sequelize from "./config/database";
+import express, { Express} from "express";
 import dotenv from "dotenv";
-import Tour from "./models/tour.model";
+import clientRoutes from "./routers/client/index.route";
 
 dotenv.config();
-sequelize; 
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 app.set('views', `./views`)
 app.set('view engine', 'pug')
 
-app.get("/tours", async (req: Request, res: Response) => { 
-    const tours = await Tour.findAll({
-        raw : true 
-    }) ;
+// clientRoutes
+clientRoutes(app);
 
-    res.render("client/pages/tours/index" , {
-        tours : tours
-    }); 
-});
 app.listen(port, () => {
     console.log(`App listening on port ${port}`); 
 });
