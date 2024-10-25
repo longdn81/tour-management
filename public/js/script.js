a55f4c2c5652e378c44b6e4c5a541b990fc0707b
longdn81 <146925221+longdn81@users.noreply.github.com>
2024-10-25 16:40:01 +0700

    them gio hang

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,59 @@ var imagesMain = new Swiper(".imagesMain", {
     },
 });
 //end slide tour detail
+
+// alert
+const alertAddCartSuccess = () => {
+    const elementAlert = document.querySelector('[alert-add-cart-success]');
+    if(elementAlert){
+        elementAlert.classList.remove("alert-hidden")
+
+        setTimeout(() => {
+            elementAlert.classList.add("alert-hidden");
+        }, 3000);
+
+        const closeAlert = elementAlert.querySelector('[close-alert]');
+        closeAlert.addEventListener("click" , () => {
+            elementAlert.classList.add("alert-hidden");
+        })
+    }
+
+}
+// end alert
+
+// cart
+const cart = localStorage.getItem("cart");
+if(!cart){
+    localStorage.setItem("cart" , JSON.stringify([])); 
+}
+// Thêm tour vào cart
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if (formAddToCart) {
+    formAddToCart.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const quantity = parseInt(event.target.elements.quantity.value);
+        const tourId = parseInt(event.target.getAttribute("tour-id"));
+
+        if (quantity > 0 && tourId) {
+            const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+            const indexExistTour = cart.findIndex(item => item.tourId === tourId);
+
+            if (indexExistTour === -1) {
+                cart.push({
+                    tourId: tourId,
+                    quantity: quantity
+                });
+            } else {
+                cart[indexExistTour].quantity = cart[indexExistTour].quantity + quantity;
+            }
+
+            localStorage.setItem("cart", JSON.stringify(cart));
+
+            alertAddCartSuccess();
+        }
+    });
+}
+
+// End carts
